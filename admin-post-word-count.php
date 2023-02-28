3cac7d4fd6df5e03627a18faf437184b2e80a417
Deepanker Verma <deepanker70@gmail.com>
2023-02-28 17:07:19 +0530

    Update admin-post-word-count.php

--- admin-post-word-count.php
+++ admin-post-word-count.php
@@ -8,24 +8,24 @@
  * Author URI: https://deepankerverma.com
  **/
 
-function post_word_count() {
+function TWG_post_word_count() {
   global $post;
   $content = $post->post_content;
   $word_count = str_word_count(strip_tags($content));
   return $word_count;
 }
 
-function post_word_count_column($columns) {
+function TWG_post_word_count_column($columns) {
   $columns['word_count'] = 'Word Count';
   return $columns;
 }
 
-function post_word_count_column_row($column_name, $post_id) {
+function TWG_post_word_count_column_row($column_name, $post_id) {
   if ($column_name == 'word_count') {
-    $word_count = post_word_count();
-    echo $word_count;
+    $word_count = TWG_post_word_count();
+    echo esc_html($word_count);
   }
 }
 
-add_filter('manage_posts_columns', 'post_word_count_column');
-add_action('manage_posts_custom_column', 'post_word_count_column_row', 10, 2);
+add_filter('manage_posts_columns', 'TWG_post_word_count_column');
+add_action('manage_posts_custom_column', 'TWG_post_word_count_column_row', 10, 2);
